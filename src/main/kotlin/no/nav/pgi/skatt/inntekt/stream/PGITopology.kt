04bb7b0a3fa1f35a8bee56b8955c69bc6459ea65
a150238 <eivind.askestad@nav.no>
2020-12-18 13:29:19 +0100

    Counter for inntekt processed and inntekt processd by year

--- src/main/kotlin/no/nav/pgi/skatt/inntekt/stream/PGITopology.kt
+++ src/main/kotlin/no/nav/pgi/skatt/inntekt/stream/PGITopology.kt
@@ -1,5 +1,6 @@
 package no.nav.pgi.skatt.inntekt.stream
 
+import io.prometheus.client.Counter
 import no.nav.pensjon.samhandling.maskfnr.maskFnr
 import no.nav.pgi.skatt.inntekt.skatt.PgiClient
 import no.nav.pgi.skatt.inntekt.stream.mapping.FetchPgiFromSkatt
@@ -8,10 +9,19 @@ import no.nav.pgi.skatt.inntekt.stream.mapping.MapToPgiAvro
 import no.nav.pgi.skatt.inntekt.stream.mapping.MapToPgiDto
 import no.nav.samordning.pgi.schema.Hendelse
 import no.nav.samordning.pgi.schema.HendelseKey
+import no.nav.samordning.pgi.schema.PensjonsgivendeInntekt
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.Topology
 import org.apache.kafka.streams.kstream.KStream
 import org.slf4j.LoggerFactory
+
+private val hendelserToinntektProcessedTotal = Counter.build()
+        .name("pgi_hendelse_to_inntekt_processed_total")
+        .help("Antall hendelser hvor det er hentet inntekt totalt").register()
+private val hendelserToinntektProcessedByYear = Counter.build()
+        .name("pgi_hendelse_to_inntekt_processed_by_year")
+        .labelNames("year")
+        .help("Antall hendelser  hvor det er hentet inntekt per år").register()
 
 internal class PGITopology(private val pgiClient: PgiClient = PgiClient()) {
 
@@ -24,6 +34,7 @@ internal class PGITopology(private val pgiClient: PgiClient = PgiClient()) {
                 .mapValues(HandleErrorCodeFromSkatt())
                 .mapValues(MapToPgiDto())
                 .mapValues(MapToPgiAvro())
+                .peek(countInntektProcessed())
                 .to(PGI_INNTEKT_TOPIC)
 
         return streamBuilder.build()
@@ -32,6 +43,11 @@ internal class PGITopology(private val pgiClient: PgiClient = PgiClient()) {
     private fun logHendelseAboutToBeProcessed(): (HendelseKey, Hendelse) -> Unit =
             { _: HendelseKey, hendelse: Hendelse -> LOG.info("Started processing hendelse ${hendelse.toString().maskFnr()}") }
 
+    private fun countInntektProcessed(): (HendelseKey, PensjonsgivendeInntekt) -> Unit =
+            { key: HendelseKey, _: PensjonsgivendeInntekt ->
+                hendelserToinntektProcessedTotal.inc()
+                hendelserToinntektProcessedByYear.labels(key.getGjelderPeriode()).inc()
+            }
 
     private companion object {
         private val LOG = LoggerFactory.getLogger(PGITopology::class.java)
