464e528a3782c79bac88d55be078cdb24c1bfd92
Marius Eriksen <marius.eriksen@nav.no>
2021-01-26 15:32:30 +0100

    Test names reflect test cases with error codes from SKE

--- src/test/kotlin/no/nav/pgi/skatt/inntekt/stream/mapping/HandleErrorCodeFromSkattTest.kt
+++ src/test/kotlin/no/nav/pgi/skatt/inntekt/stream/mapping/HandleErrorCodeFromSkattTest.kt
@@ -27,34 +27,34 @@ internal class HandleErrorCodeFromSkattTest {
         assertThrows<UnhandledStatusCodeException> { handleErrorCodesMapper.apply(mockkHttpResponse(DUMMY_BODY, 404)) }
     }
     @Test
-    internal fun `should throw UnsupportedInntektsAarException when status is not handled`() {
+    internal fun `should throw UnsupportedInntektsAarException when error message contains PGIF-005`() {
         assertThrows<UnsupportedInntektsAarException> {
             handleErrorCodesMapper.apply(mockkHttpResponse("PGIF-005", 400))
         }
     }
     @Test
-    internal fun `should throw PgiForYearAndIdentifierNotFoundException when status is not handled`() {
+    internal fun `should throw PgiForYearAndIdentifierNotFoundException when error message contains PGIF-006`() {
         assertThrows<PgiForYearAndIdentifierNotFoundException> {
             handleErrorCodesMapper.apply(mockkHttpResponse("PGIF-006", 404))
         }
     }
 
     @Test
-    internal fun `should throw InvalidInntektsAarFormatException when status is not handled`() {
+    internal fun `should throw InvalidInntektsAarFormatException when error message contains PGIF-007`() {
          assertThrows<InvalidInntektsAarFormatException> {
              handleErrorCodesMapper.apply(mockkHttpResponse("PGIF-007", 400))
          }
     }
 
     @Test
-    internal fun `should throw InvalidPersonidentifikatorFormatException when status is not handled`() {
+    internal fun `should throw InvalidPersonidentifikatorFormatException when error message contains PGIF-008`() {
         assertThrows<InvalidPersonidentifikatorFormatException> {
             handleErrorCodesMapper.apply(mockkHttpResponse("PGIF-008", 400))
         }
     }
 
     @Test
-    internal fun `should throw NoPersonWithGivenIdentifikatorException when status is not handled`() {
+    internal fun `should throw NoPersonWithGivenIdentifikatorException when error message contains PGIF-009`() {
         assertThrows<NoPersonWithGivenIdentifikatorException> {
             handleErrorCodesMapper.apply(mockkHttpResponse("PGIF-009", 404))
         }
