aaecd1cf22d001404f8946ee851f89202f9c66c7
Oyvind Buer <oyvindbuer@gmail.com>
2021-01-26 14:34:35 +0100

    Improve readability HandleErrorCodeFromSkatt

--- src/main/kotlin/no/nav/pgi/skatt/inntekt/stream/mapping/HandleErrorCodeFromSkatt.kt
+++ src/main/kotlin/no/nav/pgi/skatt/inntekt/stream/mapping/HandleErrorCodeFromSkatt.kt
@@ -1,7 +1,9 @@
 package no.nav.pgi.skatt.inntekt.stream.mapping
 
+import io.netty.handler.codec.http.HttpStatusClass
 import no.nav.pensjon.samhandling.maskfnr.maskFnr
 import org.apache.kafka.streams.kstream.ValueMapper
+import java.net.URLConnection
 import java.net.http.HttpResponse
 
 internal class HandleErrorCodeFromSkatt : ValueMapper<HttpResponse<String>, String> {
@@ -12,15 +14,17 @@ internal class HandleErrorCodeFromSkatt : ValueMapper<HttpResponse<String>, Stri
         }
     }
 
-    private fun handleError(httpResponse: HttpResponse<String>): Nothing = when {
-        httpResponse.statusCode() == 400 && httpResponse.body().contains("PGIF-005") -> throw UnsupportedInntektsAarException("LOL")
-        httpResponse.statusCode() == 400 && httpResponse.body().contains("PGIF-007") -> throw InvalidInntektsAarFormatException("LOL")
-        httpResponse.statusCode() == 400 && httpResponse.body().contains("PGIF-008") -> throw InvalidPersonidentifikatorFormatException("LOL")
-        httpResponse.statusCode() == 404 && httpResponse.body().contains("PGIF-006") -> throw PgiForYearAndIdentifierNotFoundException("LOL")
-        httpResponse.statusCode() == 404 && httpResponse.body().contains("PGIF-009") -> throw NoPersonWithGivenIdentifikatorException("LOL")
-        else -> throw UnhandledStatusCodeException("Call to pgi failed with code: ${httpResponse.statusCode()} and body: ${httpResponse.body()}")
+    private fun handleError(response: HttpResponse<String>): Nothing = when {
+        response.statusCode() == 400 && response hasErrorMessage "PGIF-005" -> throw UnsupportedInntektsAarException("PGIF-005\tDet forespurte inntektsåret er ikke støttet")
+        response.statusCode() == 400 && response hasErrorMessage "PGIF-007" -> throw InvalidInntektsAarFormatException("LOL")
+        response.statusCode() == 400 && response hasErrorMessage "PGIF-008" -> throw InvalidPersonidentifikatorFormatException("LOL")
+        response.statusCode() == 404 && response hasErrorMessage "PGIF-006" -> throw PgiForYearAndIdentifierNotFoundException("LOL")
+        response.statusCode() == 404 && response hasErrorMessage "PGIF-009" -> throw NoPersonWithGivenIdentifikatorException("LOL")
+        else -> throw UnhandledStatusCodeException("Call to pgi failed with code: ${response.statusCode()} and body: ${response.body()}")
     }
 }
+
+infix fun HttpResponse<String>.hasErrorMessage(errorMessage: String) = body().contains(errorMessage)
 
 class UnsupportedInntektsAarException(message: String) : RuntimeException(message.maskFnr())
 class PgiForYearAndIdentifierNotFoundException(message: String) : RuntimeException(message.maskFnr())
