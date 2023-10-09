27fe820f5dbfa5bb6f986b56042b023db8929a1b
Jan Kroken <jan.kroken@nav.com>
2023-10-09 09:18:24 +0200

    Oppdaterte kotlin til 1.9.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,8 @@ val mockkVerion = "1.10.6"
 group = "no.nav.pgi"
 
 plugins {
-    kotlin("jvm") version "1.8.0"
-    kotlin("plugin.serialization") version "1.8.0"
+    kotlin("jvm") version "1.9.10"
+    kotlin("plugin.serialization") version "1.9.10"
     id("com.github.ben-manes.versions") version "0.49.0"
 }
 
