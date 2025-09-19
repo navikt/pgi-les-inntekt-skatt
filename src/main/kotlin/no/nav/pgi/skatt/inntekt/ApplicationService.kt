51cbe6392ba33ecf8b88a63ea119c0c984b67b12
Arild Birkeland <arild.birkeland@nav.no>
2025-09-19 20:34:19 +0200

    schedule a restart of the app every 12 hours to handle kafka stops working (#12)

--- src/main/kotlin/no/nav/pgi/skatt/inntekt/ApplicationService.kt
+++ src/main/kotlin/no/nav/pgi/skatt/inntekt/ApplicationService.kt
@@ -1,5 +1,8 @@
 package no.nav.pgi.skatt.inntekt
 
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import kotlin.system.exitProcess
 import no.nav.pgi.skatt.inntekt.skatt.PgiClient
 import no.nav.pgi.skatt.inntekt.stream.KafkaConfig
 import no.nav.pgi.skatt.inntekt.stream.PGIStream
@@ -18,10 +21,23 @@ class ApplicationService(
             pgiClient = pgiClient
         )
     )
+    private val scheduler = Executors.newSingleThreadScheduledExecutor()
 
     internal fun start() {
         addShutdownHook()
+        scheduleShutdown()
         pgiStream.start()
+    }
+
+    private fun scheduleShutdown() {
+        val delayInHours = 12L
+
+        LOG.info("Scheduling graceful shutdown in $delayInHours hours to refresh Kafka consumer.")
+
+        scheduler.schedule({
+            LOG.info("Scheduled shutdown initiated. Exiting application to trigger pod restart.")
+            exitProcess(0)
+        }, delayInHours, TimeUnit.HOURS)
     }
 
     private fun addShutdownHook() {
@@ -30,6 +46,7 @@ class ApplicationService(
             try {
                 LOG.info("Shutdown hook triggered closing pgiStream")
                 pgiStream.close()
+                scheduler.shutdown()
             } catch (e: Exception) {
                 LOG.error("Error while shutting down", e)
             }
